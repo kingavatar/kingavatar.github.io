a2db18977e310056254704e20d9b534137687fb9
kingavatar <saikiranreddy06@gmail.com>
2021-06-30 00:52:05 +0530

    Build output as of 7394cd1b4b648e5dc5601d56da4061d9f04d7c97

---
+++ precache-manifest.b99ef29b77c23cd7b7f12d77c84d73eb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caafd2f487cab44b4c256f71c0319400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c452346f2a6935fb32b7c66d22bdccb1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d2d0cc93ec845767628f6a2841e9630c",
+    "url": "/pdf/bigdataREReport.pdf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "66eb6ea4a93606552ae9c06b237821d9",
+    "url": "/saikiranResume.pdf"
+  },
+  {
+    "revision": "af603601808ab44da6ec",
+    "url": "/static/css/app.85e21bda.css"
+  },
+  {
+    "revision": "5f4b1c217552c2549858",
+    "url": "/static/css/chunk-vendors.306287ed.css"
+  },
+  {
+    "revision": "17cb9303df7b7264b9c4ef0953366617",
+    "url": "/static/fonts/Gilroy-Bold.17cb9303.otf"
+  },
+  {
+    "revision": "3b71558ede3ea2f5583bfafd8e5ba022",
+    "url": "/static/fonts/Gilroy-Bold.3b71558e.ttf"
+  },
+  {
+    "revision": "553c3761fd307813a56d36eda6f0ad05",
+    "url": "/static/fonts/Gilroy-Bold.553c3761.eot"
+  },
+  {
+    "revision": "7b95d33fb5d943659028e591d203bbd8",
+    "url": "/static/fonts/Gilroy-Bold.7b95d33f.woff"
+  },
+  {
+    "revision": "5132ead662144e71ebd3129cb2176220",
+    "url": "/static/fonts/Gilroy-Medium.5132ead6.woff"
+  },
+  {
+    "revision": "93ccd3a55aa9df59ab012de9a439e720",
+    "url": "/static/fonts/Gilroy-Medium.93ccd3a5.eot"
+  },
+  {
+    "revision": "9e12d6053ae1c539d9be9d6fa86d33a7",
+    "url": "/static/fonts/Gilroy-Medium.9e12d605.otf"
+  },
+  {
+    "revision": "1e09f38cc59a3ccc24cd2d7d745566ac",
+    "url": "/static/fonts/Gilroy-Regular.1e09f38c.otf"
+  },
+  {
+    "revision": "5012854d2494fb7ac19c584b97a89033",
+    "url": "/static/fonts/Gilroy-Regular.5012854d.woff"
+  },
+  {
+    "revision": "de721f061724ee5f1acf1951f7385d27",
+    "url": "/static/fonts/Gilroy-Regular.de721f06.eot"
+  },
+  {
+    "revision": "57a50c1e9a16eec71ce538c86e4cab84",
+    "url": "/static/fonts/Gilroy-SemiBold.57a50c1e.eot"
+  },
+  {
+    "revision": "5b057a227c11cb0c43e7502d7c3933b6",
+    "url": "/static/fonts/Gilroy-SemiBold.5b057a22.otf"
+  },
+  {
+    "revision": "7f2c802e063d335cf0e7acc11b9cefac",
+    "url": "/static/fonts/Gilroy-SemiBold.7f2c802e.woff"
+  },
+  {
+    "revision": "39a7fbadf7e73c53d44accd9ab48a7eb",
+    "url": "/static/img/Gilroy-Bold.39a7fbad.svg"
+  },
+  {
+    "revision": "c126045d89f47105e33b096755731e9a",
+    "url": "/static/img/airline.c126045d.png"
+  },
+  {
+    "revision": "87794d644f1edcb0ec6c3f654712a5d9",
+    "url": "/static/img/back.87794d64.jpg"
+  },
+  {
+    "revision": "73c826173ce77064f2c447ef341f1501",
+    "url": "/static/img/back_mobile.73c82617.jpg"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/static/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "d6e12a74e7f4d0ff7f9eb98d22e8a235",
+    "url": "/static/img/menu.d6e12a74.png"
+  },
+  {
+    "revision": "49d60f27b701034aa4beac4ce2b8bc3d",
+    "url": "/static/img/mosip.49d60f27.png"
+  },
+  {
+    "revision": "fa3bb40e945f1b2a967697c28c17bc99",
+    "url": "/static/img/venex.fa3bb40e.png"
+  },
+  {
+    "revision": "af603601808ab44da6ec",
+    "url": "/static/js/app.3d0f5e90.js"
+  },
+  {
+    "revision": "5f4b1c217552c2549858",
+    "url": "/static/js/chunk-vendors.4090c66a.js"
+  }
+]);
